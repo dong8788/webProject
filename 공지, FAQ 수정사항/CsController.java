182d3c83067df7108cda3d3edcce7ed8cd258a4f
Hosang <brother0105@naver.com>
2020-11-15 16:44:17 +0900

    faq 수정사항 : 로그인(관리자만 쓸수있도록) 및 카트카운트 적용

---
+++ 공지, FAQ 수정사항/CsController.java
@@ -0,0 +1,287 @@
+package com.spring.webprj.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.webprj.common.Criteria;
+import com.spring.webprj.common.PageMaker;
+import com.spring.webprj.common.SearchVO;
+import com.spring.webprj.domain.CenQueryVo;
+import com.spring.webprj.domain.CustomerVo;
+import com.spring.webprj.domain.FaqVo;
+import com.spring.webprj.domain.NoticeVo;
+import com.spring.webprj.service.CartService;
+import com.spring.webprj.service.CenQueryService;
+import com.spring.webprj.service.FaqService;
+import com.spring.webprj.service.NoticeService;
+
+@Controller
+@RequestMapping("/cs")
+public class CsController {
+	
+	@Autowired
+	private NoticeService noticeservice;
+	
+	@Autowired
+	private CenQueryService cenQueryservice;
+	
+	@Autowired
+	private FaqService faqservice;
+	
+	@Autowired
+	private CartService cartservice;
+	
+	@GetMapping("/cenquery")
+	public String cenquery(SearchVO search, HttpSession session, Model model) {
+		
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+				int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+				model.addAttribute("cartSize", cartSize);
+		}
+		
+		PageMaker pm = new PageMaker();
+		pm.setCri(search);
+		System.out.println(search.getRowStart());
+		System.out.println("parameter(페이지번호) : "+search.getPage());
+		System.out.println("검색 조건 : "+ search.getCondition());
+		System.out.println("검색어 : "+ search.getKeyword());
+		System.out.println("rowStart : "+ search.getRowStart());
+		System.out.println("rowEnd : "+ search.getRowEnd());
+		
+		List<CenQueryVo> cenQueryList = cenQueryservice.list(search);
+		pm.setTotalCount(cenQueryservice.listCount(search));
+		System.out.println(cenQueryservice.list(search).size());
+		model.addAttribute("userCenQueryList", cenQueryList);
+		System.out.println(cenQueryList);
+		model.addAttribute("pageMaker", pm);
+		
+		return "cs/cenquery";
+	}
+
+	//FAQ
+	@GetMapping("/faq")
+	public String faq(Model model, HttpSession session, SearchVO search) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		PageMaker pm = new PageMaker();
+		pm.setCri(search);
+		System.out.println(search.getRowStart());
+		System.out.println("parameter(페이지번호) : "+search.getPage());
+		System.out.println("검색 조건 : "+ search.getCondition());
+		System.out.println("검색어 : "+ search.getKeyword());
+		System.out.println("rowStart : "+ search.getRowStart());
+		System.out.println("rowEnd : "+ search.getRowEnd());
+		pm.setTotalCount(faqservice.listCount(search));
+		model.addAttribute("faqList", faqservice.selectAll(search));
+		model.addAttribute("pageMaker", pm);
+		return "cs/faq/list";
+	}
+	@GetMapping("/faq/view/{seq}")
+	public String faqView(Model model, HttpSession session, @PathVariable int seq) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		FaqVo faqVo = faqservice.select(seq);
+		model.addAttribute("faqVo", faqVo);
+		return "cs/faq/view";
+	}
+	@GetMapping("/faq/insert")
+	public String faqInsert(Model model, HttpSession session) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		CustomerVo customerVo = (CustomerVo)session.getAttribute("login");
+		if(session.getAttribute("seller1") == null &&
+				customerVo.getCusId().equals("admin")){
+			model.addAttribute("faqVo", new FaqVo());
+			return "cs/faq/insert";
+		} else {
+			return "redirect:/cs/faq";
+		}
+	}
+	@PostMapping("/faq/insert")
+	public String faqInsert(Model model, HttpSession session, FaqVo faqVo) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		faqservice.insert(faqVo);
+		return "redirect:/cs/faq";
+	}
+	@GetMapping("/faq/delete/{seq}")
+	public String faqDelete(Model model, HttpSession session, @PathVariable int seq) {
+		CustomerVo customerVo = (CustomerVo)session.getAttribute("login");
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		if(session.getAttribute("login") == null) {
+			return "redirect:/cs/faq";
+		}
+		if(session.getAttribute("seller1") == null &&
+				customerVo.getCusId().equals("admin")){
+			faqservice.delete(seq);
+			return "cs/faq/delete";
+		} else {
+			return "redirect:/cs/faq";
+		}
+	}
+	@GetMapping("/faq/update/{seq}")
+	public String faqUpdate(Model model, HttpSession session, @PathVariable int seq) {
+		CustomerVo customerVo = (CustomerVo)session.getAttribute("login");
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		if(session.getAttribute("login") == null) {
+			return "redirect:/cs/faq";
+		}
+		if(session.getAttribute("seller1") == null &&
+				customerVo.getCusId().equals("admin")){
+			FaqVo faqVo = faqservice.select(seq);
+			model.addAttribute("faqVo", faqVo);
+			return "cs/faq/update";
+		} else {
+			return "redirect:/cs/faq";
+		}
+	}
+	@PostMapping("/faq/update/{seq}")
+	public String faqUpdate(FaqVo faqVo, Model model, HttpSession session, @PathVariable int seq) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		System.out.println("updated faqVo : " + faqVo);
+		faqservice.update(faqVo);
+		return "redirect:/cs/faq";
+	}
+	//FAQ 끝
+	
+	
+	//공지사항
+	@GetMapping("/notice")
+	public String notice(Model model, HttpSession session, SearchVO search) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		PageMaker pm = new PageMaker();
+		pm.setCri(search);
+		System.out.println(search.getRowStart());
+		System.out.println("parameter(페이지번호) : "+search.getPage());
+		System.out.println("검색 조건 : "+ search.getCondition());
+		System.out.println("검색어 : "+ search.getKeyword());
+		System.out.println("rowStart : "+ search.getRowStart());
+		System.out.println("rowEnd : "+ search.getRowEnd());
+		pm.setTotalCount(noticeservice.listCount(search));
+		model.addAttribute("noticeList", noticeservice.selectAll(search));
+		model.addAttribute("pageMaker", pm);
+		return "cs/notice/list";
+	}
+	@GetMapping("/notice/view/{seq}")
+	public String noticeView(Model model, HttpSession session, @PathVariable int seq) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		NoticeVo noticeVo = noticeservice.selectBySeq(seq);
+		model.addAttribute("noticeVo", noticeVo);
+		return "cs/notice/view";
+	}
+	@GetMapping("/notice/write")
+	public String noticeInsert(Model model, HttpSession session) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		CustomerVo customerVo = (CustomerVo)session.getAttribute("login");
+		if(session.getAttribute("seller1") == null &&
+				customerVo.getCusId().equals("admin")){
+			model.addAttribute("noticeVo", new NoticeVo());
+			return "cs/notice/write";
+		} else {
+			return "redirect:/cs/notice";
+		}
+	}
+	@PostMapping("/notice/write")
+	public String noticeInsert(Model model, HttpSession session, NoticeVo noticeVo) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		noticeservice.insert(noticeVo);
+		return "redirect:/cs/notice";
+	}
+	@GetMapping("/notice/delete/{seq}")
+	public String noticeDelete(Model model, HttpSession session, @PathVariable int seq) {
+		CustomerVo customerVo = (CustomerVo)session.getAttribute("login");
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		if(session.getAttribute("login") == null) {
+			return "redirect:/cs/notice";
+		}
+		if(session.getAttribute("seller1") == null &&
+				customerVo.getCusId().equals("admin")){
+			noticeservice.delete(seq);
+			return "cs/notice/delete";
+		} else {
+			return "redirect:/cs/notice";
+		}
+	}
+	@GetMapping("/notice/update/{seq}")
+	public String noticeUpdate(Model model, HttpSession session, @PathVariable int seq) {
+		CustomerVo customerVo = (CustomerVo)session.getAttribute("login");
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		if(session.getAttribute("login") == null) {
+			return "redirect:/cs/notice";
+		}
+		if(session.getAttribute("seller1") == null &&
+				customerVo.getCusId().equals("admin")){
+			NoticeVo noticeVo = noticeservice.selectBySeq(seq);
+			model.addAttribute("noticeVo", noticeVo);
+			return "cs/notice/update";
+		} else {
+			return "redirect:/cs/notice";
+		}
+	}
+	@PostMapping("/notice/update/{seq}")
+	public String noticeUpdate(Model model, HttpSession session, NoticeVo noticeVo, @PathVariable int seq) {
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		System.out.println("updated noticeVo : " + noticeVo);
+		noticeservice.update(noticeVo);
+		return "redirect:/cs/notice";
+	}
+	//공지사항 끝
+	
+	@GetMapping("/main")
+	public String main(HttpSession session, Model model){
+		if(session.getAttribute("seller1") == null && session.getAttribute("login") != null) {
+			int cartSize = cartservice.select(((CustomerVo)session.getAttribute("login")).getCusSeq()).size();
+			model.addAttribute("cartSize", cartSize);
+		}
+		return "cs/support";
+	}
+
+}
---
+++ 공지, FAQ 수정사항/faq/list.jsp
@@ -0,0 +1,90 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<jsp:include page="../../include/header.jsp" />
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="<c:url value='/styles/categories_styles.css'/>">
+</head>
+<body>
+	<div style="width:1200px;margin-left:auto;margin-right:auto">
+		<div class="sidebar" style="margin-left:50px;">
+			<div class="sidebar_section">
+				<div class="sidebar_title">
+					<h5>고객 센터</h5>
+				</div>
+				<ul class="sidebar_categories">
+					<li><a href="<c:url value='/cs/notice'/>">공지 사항</a></li>
+					<li><a href="<c:url value='/cs/faq'/>">FAQ</a></li>
+					<li><a href="<c:url value='/cs/cenquery'/>">문의 사항</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="table-wrapper" style="margin-left:250px;margin-top:200px;overflow-x:auto;max-width:70% ">
+			<div><h4>FAQ</h4></div>
+			<div  style="margin-top: 6px">
+				<table style="width:100%;">
+					<thead align="center">
+						<tr>
+							<th>번호</th>
+							<th>제목</th>
+							<th>작성자</th>					
+							<th>작성일</th>					
+							<th>조회수</th>								
+						</tr>
+					</thead>
+					<tbody align="center">
+					<c:forEach var="faq" items="${faqList}" varStatus="loop">
+						<tr style="height: 2em">
+							<td>${faq.faqSeq}</td>
+							<td>
+								<a href="<c:url value="/cs/faq/view/${faq.getFaqSeq()}"/>">
+								${faq.query}</a>
+							</td>
+							<td>운영자</td>
+							<td>${faq.regDate}</td>
+							<td></td>
+						</tr>
+					</c:forEach>
+					</tbody>
+				</table>
+			</div>
+			<hr width="100%">
+			<div align="center">
+				
+			    	<c:if test="${pageMaker.prev}">
+			    		<a href="<c:url value='/cs/faq/${pageMaker.makeQuery(pageMaker.startPage - 1)}'/>">이전</a>&nbsp;
+			    	</c:if> 
+			
+				    <c:forEach begin="${pageMaker.startPage}" end="${pageMaker.endPage}" var="idx">
+				    	<a href="<c:url value='/cs/faq/${pageMaker.makeQuery(idx)}'/>">${idx}</a>&nbsp;
+			    	</c:forEach>
+			
+				    <c:if test="${pageMaker.next && pageMaker.endPage > 0}">
+				    	<a href="<c:url value='/cs/faq/${pageMaker.makeQuery(pageMaker.endPage + 1)}'/>">다음</a>
+			   	 	</c:if> 
+			  	
+			</div>
+			<div align="right" style="margin-bottom: 3px; margin-top: 3px">
+				<select>
+					<option value="register">가입</option>
+					<option value="customer">고객</option>
+					<option value="seller">판매자</option>
+					<option value="etc">기타</option>
+				</select>
+				<input type="text" placeholder="검색" name="keyword">
+				<button type="button" onclick="location.href='/cs/faq/search'">검색하기</button>
+				<c:if test="${login.cusId == 'admin'}">
+				<button type="button" onclick="location.href='<c:url value="/cs/faq/insert"/>'">작성하기</button>
+				</c:if>	
+			</div>
+			
+		</div>
+	</div>
+	
+</body>
+</html>
+<jsp:include page="../../include/footer.jsp" />
